bc9a7ea611a269d2fe432f73b85baca34a02b9eb
Philip Alb <philip.alb@gmx.de>
2018-11-07 21:16:50 +0100

    Fixed error by adding @Transactional to Bootstrap. Fixed other error by excluding recipe from equal and hashcode methods

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -9,12 +9,14 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * Created by jt on 6/13/17.
  */
 @Entity
 @Data
+@EqualsAndHashCode(exclude = "recipes")
 public class Category {
 
 	@Id
@@ -25,27 +27,4 @@ public class Category {
 	@ManyToMany(mappedBy = "categories")
 	private Set<Recipe> recipes;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Set<Recipe> getRecipes() {
-		return recipes;
-	}
-
-	public void setRecipes(Set<Recipe> recipes) {
-		this.recipes = recipes;
-	}
 }
