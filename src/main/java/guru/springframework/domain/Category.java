245c4aaf71973cd21ee19a0b7e45fba64b0a8e65
Philip Alb <philip.alb@gmx.de>
2018-11-07 10:04:11 +0100

    Now this is the on with only one change as described in Slag

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -9,8 +9,6 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 /**
  * Created by jt on 6/13/17.
@@ -21,15 +19,27 @@ public class Category {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Getter
-	@Setter
 	private Long id;
-	@Getter
-	@Setter
 	private String description;
 
 	@ManyToMany(mappedBy = "categories")
 	private Set<Recipe> recipes;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 	public Set<Recipe> getRecipes() {
 		return recipes;
