0a4fec35fbf6615dc4b3d804d524e249ae0f56df
Philip Alb <philip.alb@gmx.de>
2018-11-07 09:29:11 +0100

    Revert "Added Project Lombok. And installed to eclipse"
    This reverts commit 9f5d3ea31094a68a44d12aa1b126fb352898e385.

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -1,36 +1,43 @@
 package guru.springframework.domain;
 
+import javax.persistence.*;
 import java.util.Set;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-
-import lombok.Data;
 
 /**
  * Created by jt on 6/13/17.
  */
 @Entity
-@Data
 public class Category {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
 
-	private Long id;
-	private String description;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
 
-	@ManyToMany(mappedBy = "categories")
-	private Set<Recipe> recipes;
+    public Long getId() {
+        return id;
+    }
 
-	public Set<Recipe> getRecipes() {
-		return recipes;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setRecipes(Set<Recipe> recipes) {
-		this.recipes = recipes;
-	}
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
 }
